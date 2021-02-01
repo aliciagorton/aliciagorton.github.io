01a21fbe3c107a5eeec74753e99586e393487907
Alicia Gorton <apersigehl@gmail.com>
2021-02-01 12:50:02 -0800

    updating project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -84,13 +84,7 @@ const Projects = () => {
                     </Typography>
                     <Grid item xs={12} sm={12} md={12} container justify="center" >
             
-                        <div 
-                            // item 
-                            // xs={12} 
-                            // sm={8} 
-                            // md={6} 
-                            className={classes.gridLayout}
-                        >
+                        <div className={classes.gridLayout}>
                             <Card className={classes.cardContainer}>
                                 <CardActionArea>
                                     <CardMedia 
@@ -104,12 +98,15 @@ const Projects = () => {
                                         Leet
                                         </Typography>
                                         <Typography variant="body2" color="textSecondary" component="p" className={classes.summary}>
-                                        Leet, an application where a user can create an account. Once login a user will be directed to a dashboard, where a number of features are available. A User will be able to create multiple user profiles, create a pdf from options provided, the ability to upload a pdf to store, and store generated pdf's.
+                                        Leet is a web application where the user creates a profile, from there they can auto-populate PDF’s. Leet offers already templated PDF’s or the ability for a user to  upload their own. 
                                         <br />
-                                        My role was designing the frontend, setting up authentication for our login, and deploying our application to heroku. This project was very challenging everything from merging on Github to deployment on Heroku. 
-                                        <br />
-                                        <br />
-                                        React -  Passport - Express - Node.js - MongoDB - JQuery - Javascript - Material UI - CSS - HTML
+                                        <ul>
+                                            <li>Led a team of 4 to design, develop, and debug, and deploy our web application.</li>
+                                            <li>Maintained gitflow workflow for application, reviewed all pull requests from team to ensure code quality and consistency. </li>
+                                            <li>Developed user credentials, authorization through passport, and deployed the web application to Heroku.</li>
+                                            <li>Front End developer using React, designed user interface, page layouts and components, used redux, and react hooks.</li>
+                                            <li>Built using React.js, Passport, Express, Node.js, MongoDBs, Material UI, third-party APIs, and JavaScript.</li>
+                                        </ul>
                                         </Typography>
                                     </CardContent>
                                     </CardActionArea>
@@ -142,14 +139,15 @@ const Projects = () => {
                                         component="p" 
                                         className={classes.summary}
                                     >
-                                    The second collaborative project from bootcamp. Kuma is an application that allows the user to create a profile for their dog in order to share and compare their pets traits in order to find man’s best friend and their own best friend.
+                                   Kuma is a web application that allows people to create profiles for their pets and compare traits with other members' pets to find playdates for them while also meeting new people with similar interests.
                                     <br />
-                                    <br />
-                                    My role was primarily front-end and design. I enjoyed learning more about bootstrap and implementing new UI designs. My primary challenge with this project was understanding sessions and how foreign keys are assigned.
-                                    <br />
-                                    <br />
-                                    Sequelize -  Passport - Express - Node.js - Handlebars - MySQL - Javascript - Bootstrap - CSS - HTML
-                                    
+                                    <ul>
+                                        <li>Collaborated with a team of 3 developers for all phases of the development life cycle, focusing on coding, testing, and debugging.</li>
+                                        <li>Using Agile methodologies and participated in weekly SCRUM meetings remotely using Zoom.  </li>
+                                        <li>Developed user credentials, web sessions and relational databases. </li>
+                                        <li>Front End developer - designed user interface, page layouts and page designs. </li>
+                                        <li>Built using Sequelize, Passport, Express, Node.js, Handlebars, MySQL, Bootstrap, and JavaScript.</li>
+                                    </ul>
                                     </Typography>
                                 </CardContent>
                                 </CardActionArea>
@@ -165,12 +163,7 @@ const Projects = () => {
                         </div>
                     
                 
-                        <div 
-                            // item 
-                            // xs={12} 
-                            // sm={8} 
-                            // md={6}
-                        >
+                        <div>
                             <Card className={classes.cardContainer}>
                                 <CardActionArea>
                                     <CardMedia 
@@ -184,14 +177,17 @@ const Projects = () => {
                                     Chuck Norris Fan Page
                                     </Typography>
                                     <Typography variant="body2" color="textSecondary" component="p" className={classes.summary}>
-                                    The first collaborative project. We went into this project wanting to thoroughly enjoy this project. Our assignment was to use two server-side APIs, a third-party API, make it interactive, and have a polished UI design.
+                                   Chuck Norris “Fan Page” with retro throwback twist, reminiscent of early web experiences and design. 
                                     <br />
-                                    <br />
-                                    My role in this project was to make our APIs functional, along with the buttons functional with JavaScript. The biggest challenge in this project was understanding the DOM and working with the APIs.
-                                    <br />
-                                    <br />
-                                    Javascript - CSS - Bulma- HTML 
-                                    
+                                    <ul>
+                                        <li> Norris “Fan Page” with retro throwback twist, reminiscent of early web experiences and design. 
+                                        Worked with a team of 3 to develop,  debug merging conflicts, and deployment with Git. 
+                                        </li>
+                                        <li>Remotely organized with weekly SCRUM meetings using  Slack and Zoom.  </li>
+                                        <li>Developed using DOM, fetch requests with APIs and creating event listeners to respond to user interactivity. </li>
+                                        <li>Contribution focused on javascript, writing functions, creating responsive events and utilizing API’s. </li>
+                                        <li>Built using two server-side APIs, a third-party API, Javascript, and Bulma.</li>
+                                    </ul>
                                     </Typography>
                                 </CardContent>
                                 </CardActionArea>
@@ -206,13 +202,8 @@ const Projects = () => {
                             </Card>
                         </div>
                         
-                        <div 
-                            // item 
-                            // xs={12} 
-                            // sm={8} 
-                            // md={6}
-                        >
-                            <Card className={classes.cardContainer}>
+                        <div>
+                            {/* <Card className={classes.cardContainer}>
                                 <CardActionArea>
                                     <CardMedia 
                                         component="img" 
@@ -240,7 +231,7 @@ const Projects = () => {
                                     Deployed
                                     </Button>
                                 </CardActions>
-                            </Card>
+                            </Card> */}
                         </div>         
                     </Grid>                     
                 </Card>
